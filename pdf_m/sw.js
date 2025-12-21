d9ea3358e59d736352a4b9e8ec0a218b38155931
jchocha <jchocha@asite.com>
2025-12-21 19:17:19 +0530

    Add PWA support.

---
+++ pdf_m/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'pdf-manipulation-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './pdf-lib.min.js',
+  './Madhav-x-ray.png',
+  './icons/android-chrome-192x192.png',
+  './icons/android-chrome-512x512.png'
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
